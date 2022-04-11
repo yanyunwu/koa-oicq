91b8e8b316559ecd049176d9798ea940606296b1
yanyun <2480721346@qq.com>
2022-04-11 19:22:17 +0800

    错误监听及上报功能

--- test2.js
+++ test2.js
@@ -5,35 +5,44 @@ const { createEvent } = KoaOicq;
 const msg = createEvent('message')
 
 msg.use(async (a, next) => {
-    console.log(11);
-    await next();
+  bb
+  console.log(11);
+  await next();
 
-    console.log(22);
+  console.log(22);
 })
 
 msg.use(async (a, next) => {
-    console.log(33);
-    await next();
-    console.log(44);
+  console.log(33);
+  await next();
+  console.log(44);
 })
 
+// msg.on('error', () => {
+//   console.log('子中间件有错误');
+// })
+
 app.use(async (a, next) => {
-    console.log(1);
-    // try {
-    await next();
-    // } catch (err) {
-    //     console.log(222);
-    // }
-    console.log(2);
+  console.log(1);
+  // try {
+  await next();
+  // } catch (err) {
+  //     console.log(222);
+  // }
+  console.log(2);
 })
 
 app.use(msg.callback())
 
 app.use(async (a, next) => {
-    aa
-    console.log(3);
-    await next();
-    console.log(4);
+  // aa
+  console.log(3);
+  await next();
+  console.log(4);
+})
+
+app.on('error', () => {
+  console.log('有错误');
 })
 
 app.listen();
